cff0e742c8bb33857d84ecbfd903d0d3cda02a02
Tirmary Reyes <tmrv1991@gmail.com>
2023-07-10 19:13:25 +0200

    Add styled component and GlobalStyle

---
+++ src/styles/GlobalStyle/theme/styled.d.ts
@@ -0,0 +1,32 @@
+import { DefaultTheme } from "styled-components";
+
+declare module "styled-components" {
+  interface DefaultTheme {
+    colors: {
+      primary: string;
+      secondary: string;
+      primaryLight: string;
+    };
+
+    fonts: {
+      primary: string;
+    };
+
+    fontSizes: {
+      medium: string;
+      big: string;
+    };
+
+    fontWeigth: {
+      regular: number;
+      medium: number;
+      bold: number;
+    };
+
+    spacing: {
+      medium: string;
+    };
+  }
+}
+
+export default DefaultTheme;
---
+++ src/styles/GlobalStyle/theme/theme.ts
@@ -0,0 +1,30 @@
+import { DefaultTheme } from "styled-components";
+
+const theme: DefaultTheme = {
+  colors: {
+    primary: "#396f84",
+    secondary: "#1A6173",
+    primaryLight: "#fff",
+  },
+
+  fonts: {
+    primary: "Montserrat, sans-serif",
+  },
+
+  fontSizes: {
+    medium: "16px",
+    big: "48px",
+  },
+
+  fontWeigth: {
+    regular: 400,
+    medium: 500,
+    bold: 700,
+  },
+
+  spacing: {
+    medium: "30px",
+  },
+};
+
+export default theme;
