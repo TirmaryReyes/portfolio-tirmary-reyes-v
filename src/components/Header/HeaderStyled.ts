8ed259554d8a44bbd2d3e3c3e9bf10d6946c4031
Tirmary Reyes <tmrv1991@gmail.com>
2023-07-11 23:37:13 +0200

    Add HeaderStyled component

---
+++ src/components/Header/HeaderStyled.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+const HeaderStyled = styled.header`
+  display: flex;
+  width: 100%;
+  align-items: center;
+  justify-content: left;
+  align-items: start;
+  flex-direction: column;
+`;
+
+export default HeaderStyled;
