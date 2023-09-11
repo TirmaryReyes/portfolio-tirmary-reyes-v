a14822b79d2ce26edbd9b98e74f52ac577279392
Tirmary Reyes <tmrv1991@gmail.com>
2023-09-11 13:28:27 +0200

    Update AboutMe component with styles

--- src/components/AboutMe/AboutMeStyled.ts
+++ src/components/AboutMe/AboutMeStyled.ts
@@ -3,41 +3,52 @@ import styled from "styled-components";
 const AboutMeStyled = styled.div`
   width: 100%;
   display: flex;
-  flex-direction: column;
-  color: #fff;
+  flex-wrap: wrap;
+  font-size: ${(props) => props.theme.fontSizes.regular};
+  color: ${(props) => props.theme.colors.secondary};
   padding: 20px;
-  background-color: #1a6173;
+  background-color: ${(props) => props.theme.colors.primary};
 
-  .container {
+  .main-container {
     display: flex;
     flex-direction: column;
     align-items: center;
-    gap: 30px;
+    gap: 18px;
 
     @media (min-width: 481px) {
-      flex-direction: row;
+      flex-direction: column;
       justify-content: space-between;
       align-items: center;
       padding: 20px;
       max-width: 800px;
       margin: 0 auto;
     }
-  }
 
-  @media (max-width: 768px) {
-    padding: 20px;
-    font-size: 16px;
-  }
+    @media (max-width: 768px) {
+      padding: 20px;
+    }
 
-  .photo {
-    display: block;
-    max-width: 100%;
-  }
+    &__graduation-cap {
+      display: block;
+      max-width: 100%;
+    }
 
-  p {
-    padding: 20px;
-    max-width: 800px;
-    margin: 0;
+    &__studies {
+      display: flex;
+      flex-wrap: wrap;
+      gap: 20px;
+      text-align: center;
+      padding: 20px;
+      max-width: 800px;
+      margin-left: 27px;
+      font-weight: ${(props) => props.theme.fontWeigth.bold};
+      font-size: ${(props) => props.theme.fontSizes.regular};
+    }
+
+    &__profession {
+      font-weight: ${(props) => props.theme.fontWeigth.bold};
+      font-size: ${(props) => props.theme.fontSizes.regular};
+    }
   }
 `;
 
