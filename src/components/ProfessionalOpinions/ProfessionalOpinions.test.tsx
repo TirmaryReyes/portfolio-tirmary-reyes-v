361bf04282bea8b53ff23006b988a402e3de3ed9
Tirmary <tmrv1991@gmail.com>
2023-10-03 13:22:55 +0300

    Fix the alignment in AboutMe component

--- src/components/ProfessionalOpinions/ProfessionalOpinions.test.tsx
+++ src/components/ProfessionalOpinions/ProfessionalOpinions.test.tsx
@@ -19,18 +19,17 @@ describe("Given a ProfessionalOpinions Component", () => {
         {
           name: "Diego Callegari",
           content: `"Tirmary is a very focused person with an exceptional eye for detail. We have worked together building CRUD web applications. Her positive attitude and concern for the group is truly commendable."`,
-          linkedIn: "https://www.linkedin.com/in/diegocallegari",
+          linkedIn: "https://www.linkedin.com/in/tirmary-reyes/",
         },
         {
           name: "Anna Marrodán Pellicer",
           content: `"Having Tirmary as a student in the Full Stack Development Bootcamp has been a pleasure. Her journey through the Bootcamp has been characterised by her resilience and adaptability, as she approached both the curriculum and coding challenges with determination and eagerness to learn."`,
-          linkedIn: "https://www.linkedin.com/in/anna-marrodan-pellicer/",
+          linkedIn: "https://www.linkedin.com/in/tirmary-reyes/",
         },
         {
           name: "Gina Marieges Rosés",
           content: `"Tirmary is a fighter, a hard worker, resilient and a empathic woman. She always take's care of the team. It has truly been a privilege to have had the opportunity to collaborate with her."`,
-          linkedIn:
-            "https://www.linkedin.com/in/gina-marieges-roses-developer/",
+          linkedIn: "https://www.linkedin.com/in/tirmary-reyes/",
         },
       ];
 
