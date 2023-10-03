97949498b0c43847e150c65bbda6eed788614318
Tirmary <tmrv1991@gmail.com>
2023-10-03 18:30:34 +0300

    Add GlobalStyle in components

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,7 +3,8 @@ import FooterStyled from "./FooterStyled";
 const Footer = (): React.ReactElement => {
   return (
     <FooterStyled>
-      <p>Built, tested, and designed by Tirmary Reyes 🍀</p>
+      <p>Built, tested, and designed by </p>
+      <p> Tirmary Reyes 🍀</p>
       <p>© {new Date().getFullYear()} All rights reserved.</p>
     </FooterStyled>
   );
